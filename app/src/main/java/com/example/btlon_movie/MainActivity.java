68d8dc3ac0722972738e204a80ab99330d7929f2
hung <hung71718@gmail.com>
2022-03-17 21:26:24 +0700

    horizontal

--- app/src/main/java/com/example/btlon_movie/MainActivity.java
+++ app/src/main/java/com/example/btlon_movie/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.btlon_movie;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -9,6 +11,7 @@ import android.os.Bundle;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager sliderPage;
     private SlidePagesAdapter MySlideAdapter;
     private TabLayout RadioSlider;
+    private RecyclerView MoveRV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         //Tham chieu
         sliderPage=findViewById(R.id.ViewPage);
         RadioSlider=findViewById(R.id.radioSelector);
+        MoveRV=findViewById(R.id.Rv_movie);
 
         //Tao Slider
         ListSlide=new ArrayList<>();
@@ -37,6 +42,18 @@ public class MainActivity extends AppCompatActivity {
         // Set Time
         Timer timer=new Timer();
         timer.scheduleAtFixedRate(new MainActivity.SliderTimer(), 4000, 6000);
+
+        //thiet lap recylerview
+        //init data
+        List<Movie> lstMovie=new ArrayList<>();
+        lstMovie.add(new Movie("The Conjuring",R.drawable.the_conjuring));
+        lstMovie.add(new Movie("The Shining",R.drawable.the_shining));
+        lstMovie.add(new Movie("Up",R.drawable.up));
+        lstMovie.add(new Movie("Your Name",R.drawable.your_name));
+
+        MovieAdapter movieAdapter=new MovieAdapter(this,lstMovie);
+        MoveRV.setAdapter(movieAdapter);
+        MoveRV.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
 
     }
     class  SliderTimer extends TimerTask{

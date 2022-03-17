b5c73761680f603bd861333619b6fb90209d1534
Nguyen Sang <nguyenvansang30102001@gmail.com>
2022-03-17 16:36:26 +0700

    Add Login Register

--- app/src/main/java/com/example/btlon_movie/MainActivity.java
+++ app/src/main/java/com/example/btlon_movie/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         RadioSlider.setupWithViewPager(sliderPage, true);
         // Set Time
         Timer timer=new Timer();
-        timer.scheduleAtFixedRate(new MainActivity.SliderTimer(), 4000, 6000);
+        timer.scheduleAtFixedRate(new MainActivity.SliderTimer(), 6000, 6000);
 
     }
     class  SliderTimer extends TimerTask{

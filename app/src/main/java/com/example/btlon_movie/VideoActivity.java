d4b5c5d3eadeacb2774e6d8ff1238ccf8ea038eb
Nguyen Sang <nguyenvansang30102001@gmail.com>
2022-03-20 19:41:30 +0700

    Add menu bottom

--- app/src/main/java/com/example/btlon_movie/VideoActivity.java
+++ app/src/main/java/com/example/btlon_movie/VideoActivity.java
@@ -1,8 +1,13 @@
 package com.example.btlon_movie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
 public class VideoActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class VideoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+        YouTubePlayerView youTubePlayerView = findViewById(R.id.VideoController);
+        getLifecycle().addObserver(youTubePlayerView);
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "Qzc_aX8c8g4";
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
     }
 }
